9f53fa5fada4e473265d41797de9b92b8c2b3a16
darock <mariuz.felea@gmail.com>
2019-12-19 14:32:53 +0200

    Admin Panel set locale based on OpenCart session

--- app/Http/Middleware/AdminPanel.php
+++ app/Http/Middleware/AdminPanel.php
@@ -5,7 +5,9 @@ namespace AwebCore\App\Http\Middleware;
 use Closure;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\View;
+use Illuminate\Foundation\Application;
 use AwebCore\Startup;
+
 class AdminPanel
 {
     /**
@@ -15,9 +17,25 @@ class AdminPanel
      * @param  \Closure  $next
      * @return mixed
      */
+    public function __construct(Application $app, Request $request) {
+        $this->app = $app;
+    }
 
     public function handle(Request $request, Closure $next)
     {
+        /**
+         * set language absed on OpenCart language sessio
+         */
+        $locale = config('app.locale');
+
+        $session = Startup::getRegistry('session');
+        if(!empty($session->data['language'])) {
+            $lang = explode('-', $session->data['language']);
+            $locale = $lang[0];
+        }
+
+        $this->app->setLocale($locale);
+
         $loader = Startup::getRegistry('load');
 
         $header = $loader->controller('common/header');
