9c9f2ae29bbd9fa2fecb2aa2ec31a7f4aeac49bb
darock <mariuz.felea@gmail.com>
2019-12-20 15:26:52 +0200

    old routes commented out temporary

--- routes/channels.php
+++ routes/channels.php
@@ -11,6 +11,7 @@
 |
 */
 
-Broadcast::channel('App.User.{id}', function ($user, $id) {
+/* Broadcast::channel('App.User.{id}', function ($user, $id) {
     return (int) $user->id === (int) $id;
 });
+ */
