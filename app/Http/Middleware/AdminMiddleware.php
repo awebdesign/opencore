16de249c1e449bee20abe1b5fb1e759962742b7e
DaRock <mariuz.felea@gmail.com>
2019-12-29 14:28:47 +0200

    Admin Middleware changes

--- app/Http/Middleware/AdminMiddleware.php
+++ app/Http/Middleware/AdminMiddleware.php
@@ -23,12 +23,22 @@ class AdminMiddleware
 
     public function handle(Request $request, Closure $next)
     {
+        if(!defined('DIR_CATALOG')) {
+            return abort(404);
+        }
+
+        $session = Startup::getRegistry('session');
+
+        /** check if OpenCart admin token is present and if is valid */
+        if(empty($session->data['token']) || empty($request->get('token')) || $session->data['token'] != $request->get('token')) {
+            return redirect(basename(DIR_APPLICATION) . '/login');
+        }
+
         /**
-         * set language absed on OpenCart language session
+         * set language based on OpenCart language session
          */
         $locale = config('app.locale');
 
-        $session = Startup::getRegistry('session');
         if(!empty($session->data['language'])) {
             $lang = explode('-', $session->data['language']);
             $locale = $lang[0];
