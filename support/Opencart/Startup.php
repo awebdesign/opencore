871d86bca1f4634a7af71f6df8a1ddf6d78c6b6a
darock <mariuz.felea@gmail.com>
2020-01-21 11:58:01 +0200

    Check route bug fix & cache system added

--- support/Opencart/Startup.php
+++ support/Opencart/Startup.php
@@ -14,7 +14,7 @@ if (!defined('DIR_APPLICATION')) {
 }
 
 if (!defined('OPENCORE_VERSION')) {
-    define('OPENCORE_VERSION', '1.2.0');
+    define('OPENCORE_VERSION', '1.2.1');
 }
 
 require_once __DIR__ . '/../../vendor/autoload.php';
@@ -24,9 +24,18 @@ use Opencore\Framework;
 
 class Startup extends \Controller
 {
-    private $route;
     private static $_registry;
+    private $route;
     private $data;
+
+    private $routes_cache_time = 3600; //1 hour expiration time
+
+    private $default_allowed_routes = [
+        'admin/core/home',
+        'admin/core/requirements',
+        'admin/core/modules',
+        'admin/core/clear-cache'
+    ];
 
     function __construct($registry)
     {
@@ -44,17 +53,26 @@ class Startup extends \Controller
      */
     public function executeIfRouteExists($route, &$data, &$output = false)
     {
+        $this->route = $route;
+        $this->data = $data;
+
         /**
          * we are using $this->request->get['route'] instead of $route because on $route some characters like dash ("-") are removed
          */
-        if (!empty($this->request->get['route']) && preg_replace('/[^a-zA-Z0-9_\/]/', '', (string) $this->request->get['route']) == $route) {
-            $route = $this->request->get['route'];
+        if (!empty($this->request->get['route']) && preg_replace('/[^a-zA-Z0-9_\/]/', '', (string) $this->request->get['route']) == $this->route) {
+            $this->route = $this->request->get['route'];
         }
 
-        if ($this->checkOpenCoreRoute($route, $data, $output)) {
+        // Strip query string (?foo=bar) and decode URI
+        if (false !== $pos = strpos($this->route, '?')) {
+            $this->route = substr($this->route, 0, $pos);
+        }
+        $this->route = rawurldecode($this->route);
+
+        if ($this->checkOpenCoreRoute()) {
             $response = $this->response();
 
-            if($output === false) {
+            if ($output === false) {
                 /**
                  * means is a controller request which means we need to use default setOutput()
                  */
@@ -75,20 +93,60 @@ class Startup extends \Controller
 
     /**
      * Check Framewrok available routes
-     *
-     * @param string $route
      */
-    public function checkOpenCoreRoute($route, &$data, &$output)
+    public function checkOpenCoreRoute()
     {
-        $this->route = $route;
-        $this->data = $data;
-
-        /*
-            We should add a dinamyc ignore list here
-        */
-        if (Framework::getInstance()->checkRoute($route, $output)) {
-            return Framework::getInstance()->handle(self::$_registry);
+        /**
+         * if is an Admin request add path to route
+         */
+        $appBaseName = basename(DIR_APPLICATION);
+        if (defined('HTTPS_CATALOG') && !strstr($this->route, $appBaseName . '/')) {
+            $this->route = $appBaseName . '/' . $this->route;
         }
+        /*
+            TODO: We should add a dynamic ignore list here which should be configurable in admin/core/settings
+        */
+
+        /**
+         * force admin route in case the request comes from admin side
+         */
+        $allowed_routes = [];
+
+        if ($this->routes_cache_time) {
+            $cache = self::$_registry->get('cache');
+            $allowed_routes = $cache->get('opencore_routes.' . $appBaseName); //separate for admin & catalog to avoid large cache files
+        }
+
+        if (in_array($this->route, $this->default_allowed_routes)) {
+            $allowed_routes[$this->route] = true;
+        }
+
+        if (isset($allowed_routes[$this->route]) && $allowed_routes[$this->route] == false) {
+            return false;
+        } else {
+            Framework::getInstance()->initiate(self::$_registry, $this->route, $output);
+
+            if (!empty($allowed_routes[$this->route])) {
+                Framework::getInstance()->initiateRouteRequest();
+            } elseif (Framework::getInstance()->checkRoute()) {
+                $allowed_routes[$this->route] = true;
+            } else {
+                $allowed_routes[$this->route] = false;
+            }
+        }
+
+        /**
+         * Cache OpenCore allowed routes for faster rendering
+         */
+        if ($this->routes_cache_time) {
+            $cache->set('opencore_routes.' . $appBaseName, $allowed_routes, time() + $this->routes_cache_time);
+        }
+
+        if ($allowed_routes[$this->route]) {
+            return Framework::getInstance()->handle();
+        }
+
+        return false;
     }
 
     /**
